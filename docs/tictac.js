ec778795c77a8c63763e38f7039858e4b0647544
narendrachn <109662555+narendrachn@users.noreply.github.com>
2023-10-12 23:21:34 -0700

    Add function for click events on game

--- docs/tictac.js
+++ docs/tictac.js
@@ -9,10 +9,43 @@
       const canvas = document.querySelector('canvas');
       const ctx = canvas.getContext('2d');
 
+      // UI references
       const restartButton = document.querySelector('#restart');
       const undoButton = document.querySelector('#undo');
-      const rotateButton = document.querySelector('#rotate');
       const playerScoreText = document.querySelector('#score-text');
+
+      // Constants
+      const CELLS_PER_AXIS = 3;
+      const CELL_WIDTH = canvas.width/CELLS_PER_AXIS;
+      const CELL_HEIGHT = canvas.height/CELLS_PER_AXIS;
+      const MAXIMUM_SCORE = CELLS_PER_AXIS * CELLS_PER_AXIS;
+
+      // Game objects
+      let grids;
+      let playerScore = MAXIMUM_SCORE;
+
+      function handleCellClick(event) {
+        const cell = event.target;
+        const cellIndex = Array.from(cells).indexOf(cell);
+
+        if (gameBoard[cellIndex] === '' && gameActive) {
+            gameBoard[cellIndex] = currentPlayer;
+            cell.innerText = currentPlayer;
+            cell.classList.add(currentPlayer);
+
+            if (checkWin()) {
+                endGame(false);
+            } else if (isBoardFull()) {
+                endGame(true);
+            } else {
+                currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+                status.innerText = `Player ${currentPlayer}'s turn`;
+            }
+        }
+    }
+
+
+
 
     }
     )
