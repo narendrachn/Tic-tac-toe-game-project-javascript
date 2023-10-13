000a7bff55d6a118fdf470d9aa172d7e3041cad1
narendrachn <109662555+narendrachn@users.noreply.github.com>
2023-10-12 23:20:11 -0700

    Add Constants that uses in the future

--- docs/tictac.js
+++ docs/tictac.js
@@ -1,1 +1,21 @@
 "use strict"
+
+(() => {
+    window.addEventListener('load', (event) => {
+      // ************************************************************************
+      // #region Constants and Variables
+  
+      // Canvas references
+      const canvas = document.querySelector('canvas');
+      const ctx = canvas.getContext('2d');
+
+      const restartButton = document.querySelector('#restart');
+      const undoButton = document.querySelector('#undo');
+      const rotateButton = document.querySelector('#rotate');
+      const playerScoreText = document.querySelector('#score-text');
+
+    }
+    )
+
+
+})();
